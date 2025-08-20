5b8d680878bb16b3d15d2f18182d14a1521f93f6
Afonso Lage <lage.afonso@gmail.com>
2025-08-20 07:48:10 -0300

    added perf

--- configuration.nix
+++ configuration.nix
@@ -16,6 +16,7 @@
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
 
+
     networking.hostName = "afonso-pc"; # Define your hostname.
     # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
@@ -74,6 +75,7 @@
         blueman
         wl-clipboard
         unzip
+        config.boot.kernelPackages.perf
     ];
 
     fonts.packages = with pkgs; [

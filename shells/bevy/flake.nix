30b4d150885f70dde00a0620d33ec04b5a80f5a8
Afonso Lage <lage.afonso@gmail.com>
2025-08-20 10:46:38 -0300

    Added tracy

--- shells/bevy/flake.nix
+++ shells/bevy/flake.nix
@@ -9,13 +9,30 @@
     outputs = { self, nixpkgs, flake-utils }:
         flake-utils.lib.eachDefaultSystem (system:
             let
-               pkgs = nixpkgs.legacyPackages.${system};
+                tracy-overlay = final: prev: {
+                  tracy-wayland = prev.tracy-wayland.overrideAttrs (oldAttrs: {
+                    pname = "tracy";
+                    version = "0.12.2";
+                    src = prev.fetchFromGitHub {
+                      owner = "wolfpld";
+                      repo = "tracy";
+                      rev = "v${oldAttrs.version}";
+                      hash = "sha256-HofqYJT1srDJ6Y1f18h7xtAbI/Gvvz0t9f0wBNnOZK8=";
+                    };
+                  });
+                };
+                pkgs = import nixpkgs {
+                    inherit system;
+                    overlays = [ tracy-overlay ];
+                };
+
                 buildInputs = with pkgs; [
                     udev
                     alsa-lib-with-plugins
                     vulkan-loader
                     wayland
                     libxkbcommon
+                    tracy-wayland
                 ];
             in
             {
